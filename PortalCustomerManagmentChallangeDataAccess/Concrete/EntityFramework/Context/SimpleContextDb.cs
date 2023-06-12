169f0738c64e1d5e0143f2790e7526ace4ba94b4
Özer <ozeruzunkaya12@gmail.com>
2023-06-13 00:45:00 +0300

    gereksiz sql bağlantıları kaldırıldı.

--- PortalCustomerManagmentChallangeDataAccess/Concrete/EntityFramework/Context/SimpleContextDb.cs
+++ PortalCustomerManagmentChallangeDataAccess/Concrete/EntityFramework/Context/SimpleContextDb.cs
@@ -8,8 +8,6 @@ namespace PortalCustomerManagmentChallangeDataAccess.Concrete.EntityFramework.Co
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=TestDb;Integrated Security=True");
-            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-F554KA6\SQLEXPRESS;Database=DemoDb;Integrated Security=true");
-            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Database=TestDatabase;Integrated Security=true");
         }
 
         public DbSet<User>? Users { get; set; }
